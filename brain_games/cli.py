0e01adea05c3474bc64ad3768c246dd79feb41d8
Eduard-Ya <eduard8116@gmail.com>
2026-01-14 22:16:50 +0500

    Добаляем конфиг сонар клауд

---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+import prompt 
+
+#Приветствует пользователя и запрашивает имя.
+def welcome_user():
+    print ('Welcome to the Brain Games!')
+    name = prompt.string("Можно узнать ваше имя?")
+    print(f'Hello, {name}!')
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,1 +1,8 @@
-print ('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
+
+#Ссылаемсся на функию велком юзер в cli.py
+def main():
+    welcome_user()
+
+if __name__ == "__main__":
+    main()
